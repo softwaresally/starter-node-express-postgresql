062f924a588333d9eef6220f360ac56f5c835130
Sally Morris <105070505+softwaresally@users.noreply.github.com>
2022-11-23 21:16:13 -0500

    added database connection

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,22 @@
 const path = require("path");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
+// const { DATABASE_URL } = process.env;
+
+const {
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
+} = process.env;
+
+const URL = 
+  NODE_ENV === "production"
+  ? PRODUCTION_DATABASE_URL
+  : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -13,4 +24,15 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
+
+  production: {
+    client: "postgresql",
+    connection: URL,
+    migrations: {
+      directory: __dirname + "/src/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/src/db/seeds"
+    }
+  }
 };
