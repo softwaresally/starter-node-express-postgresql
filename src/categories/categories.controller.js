3d75a6731e9851121e9630d0490b14def82cf8aa
Sally Morris <105070505+softwaresally@users.noreply.github.com>
2022-11-23 20:58:44 -0500

    initial commit

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,13 +1,22 @@
-async function list(req, res, next) {
-  res.json({
-    data: [
-      { category_name: "category 1" },
-      { category_name: "category 2" },
-      { category_name: "category 3" },
-    ],
-  });
+const categoriesService = require("./categories.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+// async function list(req, res, next) {
+//   const data = await categoriesService.list();
+//   res.json({ data });
+// }
+
+// module.exports = {
+//   list: asyncErrorBoundary(list),
+// };
+
+function list(req, res, next) {
+  categoriesService
+  .list()
+  .then((data) => res.json({ data }))
+  .catch(next);
 }
 
 module.exports = {
-  list: [list],
+  list,
 };
